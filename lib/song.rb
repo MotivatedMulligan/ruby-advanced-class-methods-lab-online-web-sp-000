9c89864665095662f49ea9868cd2f60669bc43e9
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-09 22:47:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,12 @@
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
 
   def self.all
     @@all
@@ -9,5 +15,43 @@ class Song
   def save
     self.class.all << self
   end
+
+def self.create(name)
+  @name = name
+  @@all << self
+  return self
+  #instantiates and saves the song, and it returns the new song that was created
+end
+def self.new_by_name
+
+  #instantiates a song with a name property
+end
+def self.create_by_name
+  #instantiates and saves a song with a name property
+end
+def self.find_by_name
+  self.all.find{|song| song.name == name}
+  #can find a song present in @@all by name
+    # returns falsey when a song name is not present in @@all
+end
+def self.find_or_create_by_name
+ #invokes .find_by_name and .create_by_name instead of repeating code
+  #returns the existing Song object (doesn't create a new one)
+    # when provided the title of an existing Song D
+  #creates a new Song object with the provided title if one doesn't already exist
+end
+def self.alphabetical
+  #returns all the song instances in alphabetical order by song name
+end
+def self.new_from_filename
+  #initializes a song and artist_name based on the filename format
+end
+def self.create_by_filename
+  #initializes and saves a song and artist_name based on the filename forma
+end
+def self.destroy_all
+  #clears all the song instances from the @@all array
+end
+
 
 end
